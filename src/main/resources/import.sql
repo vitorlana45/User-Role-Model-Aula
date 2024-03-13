16523bbfa21e019da98755e0aa7708c0b4629a35
vitorlana45 <vitoresmerio00@gmail.com>
2024-03-13 19:32:45 -0300

    BCrypt Password

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$QS7K2qmf.gyht928qMV7quNLigBG3e4mI7D4Xn0BrtUcZpJytkTNa');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$QS7K2qmf.gyht928qMV7quNLigBG3e4mI7D4Xn0BrtUcZpJytkTNa');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
